ee9b3cfbbe177efeb8fab943bb6eec0f614ee34c
Morgan Jones <morganojones@yahoo.com>
2024-11-15 18:32:37 +0000

    Update main.py

--- Turtle Crossing/main.py
+++ Turtle Crossing/main.py
@@ -29,16 +29,14 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
-# Detect collision
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             scoreboard.game_over()
             player.goto(STARTING_POSITION)
-
-# Detect reaching end
-    if player.ycor() > 280:  # Assuming the top of the screen is around y = 300
-        player.goto(0, -280)  # Move the player back to the starting position
-        car_manager.level_up()  # Increase the speed of the cars
+            
+    if player.ycor() > 280:  
+        player.goto(0, -280)  
+        car_manager.level_up() 
         scoreboard.increase_level()
         player.increase_speed()
 
